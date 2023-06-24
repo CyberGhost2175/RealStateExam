badb99b100ee51a73693c69902186ef5070d8e59
Akhan <adulatbai@bk.ru>
2023-06-25 05:39:28 +0600

    create intro activity

---
+++ app/src/main/java/com/example/realstateexamapp/Activity/IntroActivity.java
@@ -0,0 +1,35 @@
+package com.example.realstateexamapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.realstateexamapp.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        initStartButton();
+    }
+
+    private void initStartButton(){
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/realstateexamapp/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.realstateexamapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.realstateexamapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
